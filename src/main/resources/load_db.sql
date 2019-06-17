08242a41a7abf7cbd969d479d2100959289ca32a
Nuralinov Olzhas <nuralinov_ozh@mail.ru>
2019-06-17 22:28:34 +0600

    DB changed from MySQL to SQLiteDB

--- src/main/resources/load_db.sql
+++ src/main/resources/load_db.sql
@@ -1,38 +1,34 @@
-INSERT INTO PARTS
-(PART_NAME, PART_IS_NEED, PART_NUM)
-VALUES
-('Видеокарта', '0', 15),
-('Процессор', '1', 10),
-('Материнская плата', '1', 10),
-('Охлаждение', '0', 11),
-('Оперативная память', '1', 17),
-('Звуковая карта', '0', 20),
-('Сетевой адаптер', '0', 20),
-('SSD накопитель', '1', 14),
-('Жесткий диск', '1', 20),
-('Блок питания', '1', 10),
-('Корпус', '1', 10),
-('Аксессуары', '0', 10),
-('Монитор', '1', 7),
-('Клавиатура', '1', 19),
-('Мышь', '0', 30),
-('Коврик для мыши', '0', 25),
-('Наушники', '0', 18),
-('Стриминг', '0', 0),
-('Кресло', '0', 10),
-('Стол', '0', 13),
-('Атрибутика', '0', 0),
-('ИБП', '0', 0),
-('Сетевой фильтр', '0', 11),
-('Операционная система', '0', 5),
-('Офисный пакет', '0', 5),
-('Антивирус', '0', 3),
-('Моддинг', '0', 0),
-('Водяное охлаждение', '0', 25),
-('Подсветка', '0', 30),
-('Оплетка кабелей', '0', 100),
-('Вентиляторы', '0', 26),
-('SLI мостик', '0', 4),
-('Кабель питания', '0', 21),
-('Термоинтерфейс', '0', 0)
-;
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Видеокарта', '0', 15);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Процессор', '1', 10);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Материнская плата', '1', 10);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Охлаждение', '0', 11);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Оперативная память', '1', 17);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Звуковая карта', '0', 20);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Сетевой адаптер', '0', 20);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('SSD накопитель', '1', 14);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Жесткий диск', '1', 20);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Блок питания', '1', 10);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Корпус', '1', 10);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Аксессуары', '0', 10);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Монитор', '1', 7);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Клавиатура', '1', 19);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Мышь', '0', 30);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Коврик для мыши', '0', 25);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Наушники', '0', 18);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Стриминг', '0', 0);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Кресло', '0', 10);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Стол', '0', 13);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Атрибутика', '0', 0);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('ИБП', '0', 0);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Сетевой фильтр', '0', 11);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Операционная система', '0', 5);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Офисный пакет', '0', 5);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Антивирус', '0', 3);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Моддинг', '0', 0);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Водяное охлаждение', '0', 25);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Подсветка', '0', 30);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Оплетка кабелей', '0', 100);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Вентиляторы', '0', 26);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('SLI мостик', '0', 4);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Кабель питания', '0', 21);
+INSERT INTO PARTS (PART_NAME, PART_IS_NEED, PART_NUM) VALUES ('Термоинтерфейс', '0', 0);
